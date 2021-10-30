39df976664b28044c38124165aea835370e2f0d4
Sanjay Giri <sanjaygir@gmail.com>
2021-10-30 11:37:33 +0545

    added author

---
+++ grails-app/domain/grails4/Author.groovy
@@ -0,0 +1,12 @@
+package grails4
+
+class Author {
+
+    String name
+    int age
+
+
+
+    static constraints = {
+    }
+}
--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -19,4 +19,28 @@ databaseChangeLog = {
             }
         }
     }
+
+
+    changeSet(author: "user (generated)", id: "1635573092928-1") {
+        createTable(tableName: "author") {
+            column(autoIncrement: "true", name: "id", type: "BIGINT") {
+                constraints(primaryKey: "true", primaryKeyName: "authorPK")
+            }
+
+            column(name: "version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "age", type: "INT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "name", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+
+
 }
